a663235473738c034203eff0e14dce630144c617
mbeuil <mbeuil@student.42.fr>
2020-02-06 16:16:36 +0100

    Updates

---
+++ precache-manifest.72e830f4f4cfa4b69718fa3f525f5956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b54804fefd5dc2e96d7d42b7f6c3729",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "8b65324a1a730da9dac4",
+    "url": "/MonstersRolodex/static/css/main.35c53e88.chunk.css"
+  },
+  {
+    "revision": "02ced3507a426352153a",
+    "url": "/MonstersRolodex/static/js/2.ccaa872b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonstersRolodex/static/js/2.ccaa872b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b65324a1a730da9dac4",
+    "url": "/MonstersRolodex/static/js/main.a063194b.chunk.js"
+  },
+  {
+    "revision": "e5fbf05eb7b3cf2cb151",
+    "url": "/MonstersRolodex/static/js/runtime-main.2bd3aecc.js"
+  }
+]);
